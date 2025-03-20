efc5d9a919092de493b6642752824ee0eaee450d
Keith <krpbarber@gmail.com>
2025-03-19 19:44:48 -0700

    possibly fixed snack issues?

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { registerRootComponent } from 'expo';
 
 export default function App() {
   return (
@@ -21,3 +22,5 @@ const styles = StyleSheet.create({
     fontWeight: "bold"
   }
 });
+
+registerRootComponent(App);
